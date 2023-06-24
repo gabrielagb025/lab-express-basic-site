a34912cbfa668d9128cdcd6b27b3100697b2ec28
Gabriela García <ggarciaborregon@gmail.com>
2023-06-24 15:01:39 +0200

    first changes

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/views/about.html");
+})
+
+app.get("/work", (request, response) => {
+    response.sendFile(__dirname + "/views/work.html");
+})
+
+app.get("/gallery", (request, response) => {
+    response.sendFile(__dirname + "/views/gallery.html");
+})
+
+app.listen(2000, () => {
+    console.log("Server is running on port 2000");
+})
